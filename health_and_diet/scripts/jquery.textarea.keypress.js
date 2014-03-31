06d4260407eeddc2eb57b6efea1a8ecbff3a373a
zernovagg <zernovagg@gmail.com>
2014-03-31 22:50:25 +0300

    delete dragndrop

--- health_and_diet/scripts/jquery.textarea.keypress.js
+++
@@ -1,7 +0,0 @@
-﻿$(function () {
-    $("textarea").on('keypress', function (e) {
-        if (e.ctrlKey && (e.keyCode == 13 || e.keyCode == 10)) {
-            $(this).closest('form').submit();
-        }
-    });
-});
